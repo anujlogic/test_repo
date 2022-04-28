d0bb87f1a37e41b27b45e27fdcd63e7905a8ee4b
Anuj <mylogicforyou@gmail.com>
2022-04-28 12:34:54 +0530

    first commit

---
+++ test.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>submit demo</title>
+  <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+  <style type="text/css">
+      .morph div img {
+  width: 200px;
+  height: 150px;
+  -webkit-filter: grayscale(0) blur(0px);
+  filter: grayscale(0) blur(0px);
+  -webkit-transition: all 0.5s ease;
+  transition: all 0.5s ease;
+}
+
+  </style>
+</head>
+<body>
+    <div class="morph">
+        <div class="pic">
+        <img src="https://.........."></img>
+        </div>
+    </div>
+</body>
+</html>
